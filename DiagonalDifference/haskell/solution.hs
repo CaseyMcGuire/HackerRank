64434985b9677c0027550bcbbdd5d9e70d488a60
Casey McGuire <casey.jay.mcguire@gmail.com>
2015-06-21 16:06:02 -0700

    Finished DiagonalDifference in haskell

---
+++ DiagonalDifference/haskell/solution.hs
@@ -0,0 +1,21 @@
+import Data.List
+
+main :: IO()
+main = do
+  dimensions <- getLine
+  let numDimensions = read dimensions::Int
+  matrix <- sequence [getLine | _ <- [1..numDimensions]]
+  let convertedMatrix = map (\x -> map (\y -> read y::Int) x) $ map words matrix
+  print $ getDiagonalSum convertedMatrix
+  return ()
+
+getDiagonalSum :: [[Int]] -> Int
+getDiagonalSum matrix = abs (leftToRightDiagonalSum matrix - rightToLeftDiagonalSum matrix)
+  
+leftToRightDiagonalSum :: [[Int]] -> Int
+leftToRightDiagonalSum matrix = fst $ foldl' (\(acc,index) x -> (acc + (x !! index), index + 1)) (0,0) matrix
+
+rightToLeftDiagonalSum :: [[Int]] -> Int
+rightToLeftDiagonalSum matrix = fst $ foldl' (\(acc,index) x -> (acc + (x !! index), index - 1)) (0, (length matrix) -1) matrix
+
+  
